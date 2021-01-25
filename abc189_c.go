a958a5f12549fb4bab52c5a4c5d9e811cfbd60be
Kazuya Kojima <kkojima@improver.co.jp>
2021-01-26 06:09:31 +0900

    Add abc189 C

--- abc189_c.go
+++ abc189_c.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	geth "github.com/ethereum/go-ethereum/mobile"
-	"os"
-	"strconv"
-	"strings"
+	"math"
 )
 
 // ABC189 C - Mandarin Orange /
 // https://atcoder.jp/contests/abc189/tasks/abc189_c
 func abc189_c() {
+	// 食べることのできるみかんの数
+	ans := 0
+
 	var n int
-	fmt.Scanf("%d", &n)
-
-	reader := bufio.NewReader(os.Stdin)
-	line, _ := reader.ReadString('\n')
-	line = strings.TrimRight(line, "\n")
-	orange_plates := strings.Split(line, " ")
-
-	eatable := 0
-	for _, plate := range orange_plates {
-		num, _ := strconv.Atoi(plate)
-
-		x <= num
-		if 1 <=
+	fmt.Scan(&n)
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
 	}
+
+	// 左位置のindex
+	for l := 0; l < n; l++ {
+		x := a[l]
+		// 右位置のindex
+		for r := l; r < n; r++ {
+			// l, r区間の中の最小値を保存
+			x = int(math.Min(float64(x), float64(a[r])))
+			// 1つの皿から取れるみかんの数 * 皿数と現在のansで大きい方を保存
+			ans = int(math.Max(float64(x*(r-l+1)), float64(ans)))
+		}
+	}
+	fmt.Println(ans)
 }
--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
 
 func main() {
-	// abc081_b()
-	// arc110_a()
-	// arc110_a_2()
-	// abc185_a()
-	// abc185_b()
-	// abc087_b()
+	// abc049_c()
 	// abc083_b()
-	// abc186_a()
-	// abc186_b()
-	// abc088_b()
+	// abc087_b()
+	// abc081_b()
 	// abc085_b()
 	// abc085_c()
+	// abc088_b()
+	// abc185_a()
+	// abc185_b()
+	// abc186_a()
+	// abc186_b()
 	// abc188_a()
 	// abc188_b()
 	// abc188_c()
-	// abc049_c()
+	abc189_c()
+
+	// arc110_a()
+	// arc110_a_2()
+
 	// keyence2021_a()
-	abc189_b()
 }
